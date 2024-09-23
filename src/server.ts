e93e3cd0cd6d3c802c57539b19d630ff49d13201
Cesar Augusto dos Santos Junior <cesar.santos@cieepr.org.br>
2024-09-23 13:01:26 -0300

    fastify-cors

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,34 @@
 import Fastify from 'fastify';
+import FastifyCors from 'fastify-cors';
 import { PrismaClient } from '@prisma/client';
 import motoristaRoutes from './controllers/motoristaController';
 import cteRoutes from './controllers/cteController';
 import userRoutes from './controllers/LoginController';
 import RouteRomaneioStockfy from './controllers/sswController';
 
+// Inicializar o servidor Fastify
 const fastify = Fastify({ logger: true });
 const prisma = new PrismaClient();
 
 // Use a porta definida no ambiente (Render) ou 3000 como fallback
-const port = 3000;
+const port =  3000;
+
+// Habilitar CORS para tratar requisições OPTIONS
+fastify.register(FastifyCors, {
+  origin: true, // Ou configure o domínio de origem específico
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Habilita métodos HTTP
+});
 
 // Rotas
 motoristaRoutes(fastify, prisma);
 cteRoutes(fastify, prisma);
 userRoutes(fastify, prisma);
 RouteRomaneioStockfy(fastify, prisma);
+
+// Rota padrão para verificar se o servidor está rodando
+fastify.get('/', async (request, reply) => {
+  reply.send({ status: 'Servidor rodando corretamente' });
+});
 
 // Iniciar o servidor
 const start = async () => {
