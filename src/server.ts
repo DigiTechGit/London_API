5444c95cf4a26f7fbb2c0e49aae16f99d3aa312b
Cesar Augusto dos Santos Junior <cesar.santos@cieepr.org.br>
2024-09-23 12:53:21 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,8 @@ import RouteRomaneioStockfy from './controllers/sswController';
 const fastify = Fastify({ logger: true });
 const prisma = new PrismaClient();
 
+// Use a porta definida no ambiente (Render) ou 3000 como fallback
 const port = 3000;
-
 
 // Rotas
 motoristaRoutes(fastify, prisma);
@@ -20,11 +20,13 @@ RouteRomaneioStockfy(fastify, prisma);
 // Iniciar o servidor
 const start = async () => {
   try {
-    await fastify.listen({ port });
-    console.log('Servidor rodando na porta 3000');
+    // Certifique-se de que o servidor escute em '0.0.0.0'
+    await fastify.listen({ port, host: '0.0.0.0' });
+    console.log(`Servidor rodando na porta ${port}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 };
+
 start();
