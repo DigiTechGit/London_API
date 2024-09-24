d57a70243ec1521d7ae91ee47411763b9f5b66cd
Cesar <cesaraugustojunior1234@gmail.com>
2024-09-24 15:35:42 -0300

    text

--- src/controllers/sswController.ts
+++ src/controllers/sswController.ts
@@ -1,46 +1,60 @@
-import { PrismaClient } from '@prisma/client';
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
-import { endpoints } from '../utils/API';
-const fetch = require('node-fetch');
+import { PrismaClient } from "@prisma/client";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { endpoints } from "../utils/API";
+const fetch = require("node-fetch");
 
-export default function RouteRomaneioStockfy(fastify: FastifyInstance, prisma: PrismaClient) {
-	fastify.options('*', (request, reply) => {
-		reply.header('Access-Control-Allow-Origin', '*');
-		reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-		reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-		reply.send();
-	});
+export default function RouteRomaneioStockfy(
+  fastify: FastifyInstance,
+  prisma: PrismaClient
+) {
+  fastify.options("*", (request, reply) => {
+    reply.header("Access-Control-Allow-Origin", "*");
+    reply.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    reply.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    reply.send();
+  });
 
-	fastify.get('/RouteRomaneioStockfy', async (request: FastifyRequest, reply: FastifyReply) => {
-	  try {
-		const token = request.headers.authorization;
-		const { siglaEnt } = request.query as { siglaEnt: string }; 
-	
-		if (!token || siglaEnt === undefined) {
-		  reply.code(401).send({ error: 'Authorization token is required' });
-		  return;
-		}
-		
-		const url = `${endpoints.roteirizaRomaneioStockfy}?siglaEnt=${siglaEnt}`;
+  fastify.get(
+    "/RouteRomaneioStockfy",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const token = request.headers.authorization;
+        const { siglaEnt } = request.query as { siglaEnt: string };
 
-		const response = await fetch(url, {
-		  method: 'GET',
-		  headers: {
-			'Authorization': token,
-			'Content-Type': 'application/json',
-		  },
-		});
+        if (!token || siglaEnt === undefined) {
+          reply.code(401).send({ error: "Authorization token is required" });
+          return;
+        }
 
-		reply.code(200).send(await response.json()); 
-	  } catch (error: any) {
-		if (error.response && error.response.data) {
-		  // Tratamento de erro da API externa
-		  reply.code(400).send({ error: error.response.data.mensagem || 'Failed to route romaneio' });
-		} else {
-		  // Tratamento de erro genérico
-		  reply.code(500).send({ error: 'Request failed' });
-		}
-	  }
-	});
-  }
-  
+        const url = `${endpoints.roteirizaRomaneioStockfy}?siglaEnt=${siglaEnt}`;
+
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
+        });
+				const text = await response.text()
+
+        reply.code(200).send(text);
+      } catch (error: any) {
+        console.log(error);
+        if (error.response && error.response.data) {
+          // Tratamento de erro da API externa
+          reply
+            .code(400)
+            .send({
+              error: error.response.data.mensagem || "Failed to route romaneio",
+            });
+        } else {
+          // Tratamento de erro genérico
+          reply.code(500).send({ error: "Request failed" });
+        }
+      }
+    }
+  );
+}
