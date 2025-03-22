685212fd101c16dc8bce2d235acc66e9e9bc8b41
Ubiratã <ubirata_lucas03@hotmail.com>
2025-03-22 09:09:30 -0300

    refatoração nf

--- src/controllers/notaFiscalController.ts
+++ src/controllers/notaFiscalController.ts
@@ -95,195 +95,293 @@ export default function notaFiscalController(
       }
     },
   );
-
   const generatePDF = async (data: any) => {
-    const doc = new PDFDocument({ margin: 30 });
+    const doc = new PDFDocument({ margins: { top: 20, bottom: 0, left: 30, right: 30 } });
     let buffers: any = [];
     const motorista = data.motorista;
     const Nfes = data.Nfes;
     let currentPage = 1;
-    // Adiciona buffers para armazenar o conteúdo do PDF
     doc.on("data", buffers.push.bind(buffers));
-
-    // Finaliza o PDF e concatena os buffers
     doc.on("end", () => {
-      console.log("PDF gerado");
+        console.log("PDF gerado");
     });
 
-    doc
-      .fontSize(7)
-      .text(`PLACA: ${motorista.placa}`, { continued: true, align: "left" })
-      .text(` CPF: ${cpfMask(data.motorista.cpf)}`, {
-        continued: true,
-        align: "left",
-      })
-      .text(` NOME: ${data.motorista.nome}`, {
-        align: "left",
-      });
-    doc.moveDown(0.5); // Espaço extra após a linha
-    doc
-      .moveTo(doc.page.margins.left, doc.y)
-      .lineTo(doc.page.width - doc.page.margins.right, doc.y)
-      .stroke();
-    doc.moveDown(0.5); // Espaço extra após a linha
-    // Loop pelas paradas
+    doc.fontSize(7).text(`PLACA: ${motorista.placa}`, { continued: true, align: "left" })
+        .text(` CPF: ${cpfMask(data.motorista.cpf)}`, { continued: true, align: "left" })
+        .text(` NOME: ${data.motorista.nome}`, { align: "left" });
+    doc.moveDown(1);
+    doc.moveTo(doc.page.margins.left, doc.y).lineTo(doc.page.width - doc.page.margins.right, doc.y).stroke();
+    doc.moveDown(1);
+
     for (let i = 0; i < Nfes.length; i++) {
-      const indice = i + 1;
-      const stops = Nfes[i];
-      const quebraPagina = indice % 4 === 0;
-      for (const stop of stops) {
-        // Título
-        // if (currentPage > 1) doc.moveDown(2);
-        doc
-          // .moveTo(doc.page.margins.left, doc.y)
-          .fontSize(7)
-          .text(`NF: ${stop.nrNfre}`, { continued: true, align: "left" })
-          .fontSize(7)
-          .text(` REMETENTE: ${stop.remetente}`, {
-            continued: true,
-            align: "center",
-          })
-          .fontSize(7)
-          .text(` PREVISÃO ENTREGA: ${stop.prevEntrega}`, { align: "right" });
-        doc.moveDown(0.2);
-        doc
-          .fontSize(7)
-          .text(`QTD VOLUMES: ${stop.qtdeVolumes}`, { align: "left" });
+        const indice = i + 1;
+        const stops = Nfes[i];
+        const quebraPagina = indice % 4 === 0;
 
-        doc.fontSize(7).text(`DESTINATÁRIO: ${stop.destinatario}`);
-        doc.moveDown(0.2);
+        for (const stop of stops) {
+            doc.fontSize(7).text(`NF: ${stop.nrNfre}`, { continued: true, align: "left" })
+                .text(` REMETENTE: ${stop.remetente}`, { continued: true, align: "center" })
+                .text(` PREVISÃO ENTREGA: ${stop.prevEntrega}`, { align: "right" });
+            doc.moveDown(0.5);
+            doc.fontSize(7).text(`QTD VOLUMES: ${stop.qtdeVolumes}`, { align: "left" });
+            doc.fontSize(7).text(`DESTINATÁRIO: ${stop.destinatario}`);
+            doc.moveDown(0.5);
+            doc.fontSize(7).text(`TELEFONE: ${stop.telefone || "N INF."} / ${stop.celular || "N INF."}`);
+            doc.moveDown(0.5);
+            doc.fontSize(7).text(`BAIRRO: ${stop.bairro}`, { align: "left" });
 
-        doc
-          .fontSize(7)
-          .text(
-            `TELEFONE: ${stop.telefone || "N INF."} / ${
-              stop.celular || "N INF."
-            }`,
-          );
-        doc.moveDown(0.2);
+            if (stop.chaveNFe || stop.chaveCTe) {
+                const chave = stop.chaveNFe && stop.chaveNFe.trim() !== "" ? stop.chaveNFe : stop.chaveCTe;
+                const qrCodeBuffer = await generateQRCode(chave);
+                doc.image(qrCodeBuffer, doc.page.width - doc.page.margins.right - 300, doc.y - 30, { fit: [60, 60] });
+                const barcodeBuffer = await generateBarcode(chave);
+                doc.image(barcodeBuffer, doc.page.width - doc.page.margins.right - 170, doc.y - 20, { fit: [170, 60] });
+            }
 
-        doc.fontSize(7).text(`BAIRRO: ${stop.bairro}`, { align: "left" });
-        if (stop.chaveNFe || stop.chaveCTe) {
-          const chave = stop.chaveNFe && stop.chaveNFe.trim() !== "" ? stop.chaveNFe : stop.chaveCTe;
-          const qrCodeBuffer = await generateQRCode(chave);
-          doc.image(
-            qrCodeBuffer,
-            doc.page.width - doc.page.margins.right - 300,
-            doc.y - 30,
-            {
-              fit: [60, 60], // Tamanho do QR Code
-              align: "center",
-              valign: "top",
-            },
-          );
+            doc.moveDown(0.5);
+            doc.fontSize(7).text(`CEP: ${stop.cep}`, { continued: true, align: "left" })
+                .text(` ${stop.cidade} / ${stop.uf}`);
+            doc.moveDown(0.5);
+            doc.fontSize(7).text(`ENDEREÇO: ${stop.endereco} ${stop.numero}`);
+            doc.moveDown(0.5);
+            doc.fontSize(7).text(`COMPL.: ${stop.complemento || ""}`);
+            doc.moveDown(2);
 
-          const barcodeBuffer = await generateBarcode(chave);
-          doc.image(
-            barcodeBuffer,
-            doc.page.width - doc.page.margins.right - 170,
-            doc.y - 20,
-            {
-              fit: [170, 60], // Novo tamanho do código de barras
-              align: "right", // Mantém à direita
-              valign: "top", // Alinha no topo
-            },
-          );
+            const lineWidth = 100;
+            doc.moveTo(doc.page.width - doc.page.margins.right - lineWidth, doc.y - 2)
+                .lineTo(doc.page.width - doc.page.margins.right, doc.y - 2).stroke();
+            doc.fontSize(7).text(`DATA`, { align: "right" });
+            doc.moveDown(2);
+
+            const lineWidthRecebedor = 120;
+            doc.moveTo(doc.page.margins.left, doc.y - 2)
+                .lineTo(doc.page.margins.left + lineWidthRecebedor, doc.y - 2).stroke();
+            doc.fontSize(7).text(`RECEBEDOR`, { continued: true, align: "left" });
+
+            const lineWidthDocumento = 120;
+            const offset = 20;
+            doc.moveTo(doc.page.width / 2 - lineWidthDocumento / 2 + offset, doc.y - 2)
+                .lineTo(doc.page.width / 2 + lineWidthDocumento / 2 + offset, doc.y - 2).stroke();
+            doc.fontSize(7).text(`DOCUMENTO`, { continued: true, align: "center" });
+
+            const lineWidthParentesco = 120;
+            doc.moveTo(doc.page.width - doc.page.margins.right - lineWidthParentesco, doc.y - 2)
+                .lineTo(doc.page.width - doc.page.margins.right, doc.y - 2).stroke();
+            doc.fontSize(7).text(`GRAU DE PARENTESCO`, { align: "right" });
+
+
+            if(!quebraPagina){
+              doc.moveDown(4);
+              doc.moveTo(doc.page.margins.left, doc.y).lineTo(doc.page.width - doc.page.margins.right, doc.y).stroke();
+              doc.moveDown(3);
+            } else {
+              doc.fontSize(7).text(`Página ${currentPage}`, { align: "center" });
+              if (indice < Nfes.length) doc.addPage();
+                currentPage++;
+            }
         }
-        doc.moveDown(0.2);
-
-        doc
-          .fontSize(7)
-          .text(`CEP: ${stop.cep}`, { continued: true, align: "left" })
-          .fontSize(7)
-          .text(` ${stop.cidade} / ${stop.uf}`);
-        doc.moveDown(0.2);
-
-        doc.fontSize(7).text(`ENDEREÇO: ${stop.endereco} ${stop.numero}`);
-        doc.moveDown(0.2);
-
-        doc.fontSize(7).text(`COMPL.: ${stop.complemento || ""}`);
-        doc.moveDown(0.5);
-
-        // Reduz o tamanho da linha, deixando-a mais curta
-        const lineWidth = 100; // Largura da linha menor
-        doc
-          .moveTo(
-            doc.page.width - doc.page.margins.right - lineWidth,
-            doc.y - 2,
-          )
-          .lineTo(doc.page.width - doc.page.margins.right, doc.y - 2)
-          .stroke();
-        doc.fontSize(7).text(`DATA`, { align: "right" });
-        doc.moveDown(2);
-
-        const lineWidthRecebedor = 120; // Defina o tamanho desejado da linha
-        doc
-          .moveTo(doc.page.margins.left, doc.y - 2)
-          .lineTo(doc.page.margins.left + lineWidthRecebedor, doc.y - 2)
-          .stroke();
-        doc.fontSize(7).text(`RECEBEDOR`, { continued: true, align: "left" });
-
-        // Linha e texto para "DOCUMENTO"
-        const lineWidthDocumento = 120; // Defina o tamanho desejado da linha
-        const offset = 20; // Ajuste para mover o traço à direita
-        doc
-          .moveTo(
-            doc.page.width / 2 - lineWidthDocumento / 2 + offset,
-            doc.y - 2,
-          ) // Centraliza e ajusta para a direita
-          .lineTo(
-            doc.page.width / 2 + lineWidthDocumento / 2 + offset,
-            doc.y - 2,
-          )
-          .stroke();
-        doc.fontSize(7).text(`DOCUMENTO`, { continued: true, align: "center" });
-
-        // Linha e texto para "GRAU DE PARENTESCO"
-        const lineWidthParentesco = 120; // Defina o tamanho desejado da linha
-        doc
-          .moveTo(
-            doc.page.width - doc.page.margins.right - lineWidthParentesco,
-            doc.y - 2,
-          )
-          .lineTo(doc.page.width - doc.page.margins.right, doc.y - 2)
-          .stroke();
-        doc.fontSize(7).text(`GRAU DE PARENTESCO`, { align: "right" });
-
-        doc.moveDown(quebraPagina ? 2 : 5); // Espaço extra após a linha
-        if (!quebraPagina) {
-          doc
-            .moveTo(doc.page.margins.left, doc.y)
-            .lineTo(doc.page.width - doc.page.margins.right, doc.y)
-            .stroke();
-          doc.moveDown(5); // Espaço extra após a linha
-        }
-      }
-
-      if (quebraPagina) {
-        doc.fontSize(7).text(`Página ${currentPage}`, {
-          align: "center",
-          baseline: "bottom",
-        });
-        if (indice < Nfes.length) doc.addPage();
-
-        currentPage++;
-      }
     }
 
-    // Finalizar o documento
     doc.end();
 
-    // Retorna o PDF como base64
     return new Promise((resolve, reject) => {
-      doc.on("end", () => {
-        const pdfBuffer = Buffer.concat(buffers);
-        const base64PDF = pdfBuffer.toString("base64");
-        resolve(base64PDF);
-      });
-
-      doc.on("error", reject);
+        doc.on("end", () => {
+            const pdfBuffer = Buffer.concat(buffers);
+            const base64PDF = pdfBuffer.toString("base64");
+            resolve(base64PDF);
+        });
+        doc.on("error", reject);
     });
-  };
+};
+
+  // const generatePDF = async (data: any) => {
+  //   const doc = new PDFDocument({ margin: 30 });
+  //   let buffers: any = [];
+  //   const motorista = data.motorista;
+  //   const Nfes = data.Nfes;
+  //   let currentPage = 1;
+  //   // Adiciona buffers para armazenar o conteúdo do PDF
+  //   doc.on("data", buffers.push.bind(buffers));
+
+  //   // Finaliza o PDF e concatena os buffers
+  //   doc.on("end", () => {
+  //     console.log("PDF gerado");
+  //   });
+
+  //   doc
+  //     .fontSize(7)
+  //     .text(`PLACA: ${motorista.placa}`, { continued: true, align: "left" })
+  //     .text(` CPF: ${cpfMask(data.motorista.cpf)}`, {
+  //       continued: true,
+  //       align: "left",
+  //     })
+  //     .text(` NOME: ${data.motorista.nome}`, {
+  //       align: "left",
+  //     });
+  //   doc.moveDown(0.5); // Espaço extra após a linha
+  //   doc
+  //     .moveTo(doc.page.margins.left, doc.y)
+  //     .lineTo(doc.page.width - doc.page.margins.right, doc.y)
+  //     .stroke();
+  //   doc.moveDown(0.5); // Espaço extra após a linha
+  //   // Loop pelas paradas
+  //   for (let i = 0; i < Nfes.length; i++) {
+  //     const indice = i + 1;
+  //     const stops = Nfes[i];
+  //     const quebraPagina = indice % 4 === 0;
+  //     for (const stop of stops) {
+  //       // Título
+  //       // if (currentPage > 1) doc.moveDown(2);
+  //       doc
+  //         // .moveTo(doc.page.margins.left, doc.y)
+  //         .fontSize(7)
+  //         .text(`NF: ${stop.nrNfre}`, { continued: true, align: "left" })
+  //         .fontSize(7)
+  //         .text(` REMETENTE: ${stop.remetente}`, {
+  //           continued: true,
+  //           align: "center",
+  //         })
+  //         .fontSize(7)
+  //         .text(` PREVISÃO ENTREGA: ${stop.prevEntrega}`, { align: "right" });
+  //       doc.moveDown(0.2);
+  //       doc
+  //         .fontSize(7)
+  //         .text(`QTD VOLUMES: ${stop.qtdeVolumes}`, { align: "left" });
+
+  //       doc.fontSize(7).text(`DESTINATÁRIO: ${stop.destinatario}`);
+  //       doc.moveDown(0.2);
+
+  //       doc
+  //         .fontSize(7)
+  //         .text(
+  //           `TELEFONE: ${stop.telefone || "N INF."} / ${
+  //             stop.celular || "N INF."
+  //           }`,
+  //         );
+  //       doc.moveDown(0.2);
+
+  //       doc.fontSize(7).text(`BAIRRO: ${stop.bairro}`, { align: "left" });
+  //       if (stop.chaveNFe || stop.chaveCTe) {
+  //         const chave = stop.chaveNFe && stop.chaveNFe.trim() !== "" ? stop.chaveNFe : stop.chaveCTe;
+  //         const qrCodeBuffer = await generateQRCode(chave);
+  //         doc.image(
+  //           qrCodeBuffer,
+  //           doc.page.width - doc.page.margins.right - 300,
+  //           doc.y - 30,
+  //           {
+  //             fit: [60, 60], // Tamanho do QR Code
+  //             align: "center",
+  //             valign: "top",
+  //           },
+  //         );
+
+  //         const barcodeBuffer = await generateBarcode(chave);
+  //         doc.image(
+  //           barcodeBuffer,
+  //           doc.page.width - doc.page.margins.right - 170,
+  //           doc.y - 20,
+  //           {
+  //             fit: [170, 60], // Novo tamanho do código de barras
+  //             align: "right", // Mantém à direita
+  //             valign: "top", // Alinha no topo
+  //           },
+  //         );
+  //       }
+  //       doc.moveDown(0.2);
+
+  //       doc
+  //         .fontSize(7)
+  //         .text(`CEP: ${stop.cep}`, { continued: true, align: "left" })
+  //         .fontSize(7)
+  //         .text(` ${stop.cidade} / ${stop.uf}`);
+  //       doc.moveDown(0.2);
+
+  //       doc.fontSize(7).text(`ENDEREÇO: ${stop.endereco} ${stop.numero}`);
+  //       doc.moveDown(0.2);
+
+  //       doc.fontSize(7).text(`COMPL.: ${stop.complemento || ""}`);
+  //       doc.moveDown(0.5);
+
+  //       // Reduz o tamanho da linha, deixando-a mais curta
+  //       const lineWidth = 100; // Largura da linha menor
+  //       doc
+  //         .moveTo(
+  //           doc.page.width - doc.page.margins.right - lineWidth,
+  //           doc.y - 2,
+  //         )
+  //         .lineTo(doc.page.width - doc.page.margins.right, doc.y - 2)
+  //         .stroke();
+  //       doc.fontSize(7).text(`DATA`, { align: "right" });
+  //       doc.moveDown(2);
+
+  //       const lineWidthRecebedor = 120; // Defina o tamanho desejado da linha
+  //       doc
+  //         .moveTo(doc.page.margins.left, doc.y - 2)
+  //         .lineTo(doc.page.margins.left + lineWidthRecebedor, doc.y - 2)
+  //         .stroke();
+  //       doc.fontSize(7).text(`RECEBEDOR`, { continued: true, align: "left" });
+
+  //       // Linha e texto para "DOCUMENTO"
+  //       const lineWidthDocumento = 120; // Defina o tamanho desejado da linha
+  //       const offset = 20; // Ajuste para mover o traço à direita
+  //       doc
+  //         .moveTo(
+  //           doc.page.width / 2 - lineWidthDocumento / 2 + offset,
+  //           doc.y - 2,
+  //         ) // Centraliza e ajusta para a direita
+  //         .lineTo(
+  //           doc.page.width / 2 + lineWidthDocumento / 2 + offset,
+  //           doc.y - 2,
+  //         )
+  //         .stroke();
+  //       doc.fontSize(7).text(`DOCUMENTO`, { continued: true, align: "center" });
+
+  //       // Linha e texto para "GRAU DE PARENTESCO"
+  //       const lineWidthParentesco = 120; // Defina o tamanho desejado da linha
+  //       doc
+  //         .moveTo(
+  //           doc.page.width - doc.page.margins.right - lineWidthParentesco,
+  //           doc.y - 2,
+  //         )
+  //         .lineTo(doc.page.width - doc.page.margins.right, doc.y - 2)
+  //         .stroke();
+  //       doc.fontSize(7).text(`GRAU DE PARENTESCO`, { align: "right" });
+
+  //       doc.moveDown(quebraPagina ? 2 : 5); // Espaço extra após a linha
+  //       if (!quebraPagina) {
+  //         doc
+  //           .moveTo(doc.page.margins.left, doc.y)
+  //           .lineTo(doc.page.width - doc.page.margins.right, doc.y)
+  //           .stroke();
+  //         doc.moveDown(5); // Espaço extra após a linha
+  //       }
+  //     }
+
+  //     if (quebraPagina) {
+  //       doc.fontSize(7).text(`Página ${currentPage}`, {
+  //         align: "center",
+  //         baseline: "bottom",
+  //       });
+  //       if (indice < Nfes.length) doc.addPage();
+
+  //       currentPage++;
+  //     }
+  //   }
+
+  //   // Finalizar o documento
+  //   doc.end();
+
+  //   // Retorna o PDF como base64
+  //   return new Promise((resolve, reject) => {
+  //     doc.on("end", () => {
+  //       const pdfBuffer = Buffer.concat(buffers);
+  //       const base64PDF = pdfBuffer.toString("base64");
+  //       resolve(base64PDF);
+  //     });
+
+  //     doc.on("error", reject);
+  //   });
+  // };
 
   const generateBarcode = async (text: any) => {
     return new Promise((resolve, reject) => {
